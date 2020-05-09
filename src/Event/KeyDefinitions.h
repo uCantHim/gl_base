11402e6f08bd555bda76861112290cb08782fa8d
Nicola Crivellin <nicola.crivellin98@gmail.com>
2020-05-09 14:31:16 +0200

    Add more key enums
    I should have all keys now

--- src/Event/KeyDefinitions.h
+++ src/Event/KeyDefinitions.h
@@ -62,31 +62,84 @@ namespace glb
         f11 = GLFW_KEY_F11,
         f12 = GLFW_KEY_F12,
 
-        left_shift	= GLFW_KEY_LEFT_SHIFT,
-        right_shift	= GLFW_KEY_RIGHT_SHIFT,
-        left_alt	= GLFW_KEY_LEFT_ALT,
-        right_alt	= GLFW_KEY_RIGHT_ALT,
-        left_ctrl	= GLFW_KEY_LEFT_CONTROL,
-        right_ctrl	= GLFW_KEY_RIGHT_CONTROL,
+        numpad_0 = GLFW_KEY_KP_0,
+        numpad_1 = GLFW_KEY_KP_1,
+        numpad_2 = GLFW_KEY_KP_2,
+        numpad_3 = GLFW_KEY_KP_3,
+        numpad_4 = GLFW_KEY_KP_4,
+        numpad_5 = GLFW_KEY_KP_5,
+        numpad_6 = GLFW_KEY_KP_6,
+        numpad_7 = GLFW_KEY_KP_7,
+        numpad_8 = GLFW_KEY_KP_8,
+        numpad_9 = GLFW_KEY_KP_9,
 
-        tab			= GLFW_KEY_TAB,
-        backspace	= GLFW_KEY_BACKSPACE,
-        capslock	= GLFW_KEY_CAPS_LOCK,
-        escape		= GLFW_KEY_ESCAPE,
+        num_lock        = GLFW_KEY_NUM_LOCK,
+        numpad_divide   = GLFW_KEY_KP_DIVIDE,
+        numpad_multiply = GLFW_KEY_KP_MULTIPLY,
+        numpad_subtract = GLFW_KEY_KP_SUBTRACT,
+        numpad_add      = GLFW_KEY_KP_ADD,
+        numpad_enter    = GLFW_KEY_KP_ENTER,
 
-        UNKNOWN_KEY
+        left_shift      = GLFW_KEY_LEFT_SHIFT,
+        right_shift     = GLFW_KEY_RIGHT_SHIFT,
+        left_alt        = GLFW_KEY_LEFT_ALT,
+        right_alt       = GLFW_KEY_RIGHT_ALT,
+        left_ctrl       = GLFW_KEY_LEFT_CONTROL,
+        right_ctrl      = GLFW_KEY_RIGHT_CONTROL,
+        left_super      = GLFW_KEY_LEFT_SUPER,
+        right_super     = GLFW_KEY_RIGHT_SUPER,
+        menu            = GLFW_KEY_MENU,
+
+        space           = GLFW_KEY_SPACE,
+        tab             = GLFW_KEY_TAB,
+        backspace       = GLFW_KEY_BACKSPACE,
+        capslock        = GLFW_KEY_CAPS_LOCK,
+        escape          = GLFW_KEY_ESCAPE,
+
+        grave_accent    = GLFW_KEY_GRAVE_ACCENT,      // ^ on german
+        minus           = GLFW_KEY_MINUS,             // ß on german
+        equal           = GLFW_KEY_EQUAL,             // ´ on german
+        backslash       = GLFW_KEY_BACKSLASH,         // non-existent key on german?
+        left_bracket    = GLFW_KEY_LEFT_BRACKET,      // ü on german
+        right_bracket   = GLFW_KEY_RIGHT_BRACKET,     // + on german
+        semicolon       = GLFW_KEY_SEMICOLON,         // ö on german
+        apostrophe      = GLFW_KEY_APOSTROPHE,        // ä on german
+        comma           = GLFW_KEY_COMMA,             // same on german
+        period          = GLFW_KEY_PERIOD,            // same on german
+        slash           = GLFW_KEY_SLASH,             // - on german
+
+        arrow_up        = GLFW_KEY_UP,
+        arrow_left      = GLFW_KEY_LEFT,
+        arrow_right     = GLFW_KEY_RIGHT,
+        arrow_down      = GLFW_KEY_DOWN,
+
+        ins             = GLFW_KEY_INSERT,
+        del             = GLFW_KEY_DELETE,
+        home            = GLFW_KEY_HOME,              // pos1 on german
+        end             = GLFW_KEY_END,
+        page_up         = GLFW_KEY_PAGE_UP,
+        page_down       = GLFW_KEY_PAGE_DOWN,
+
+        print           = GLFW_KEY_PRINT_SCREEN,      // druck on german
+        scroll_lock     = GLFW_KEY_SCROLL_LOCK,       // rollen on german
+        pause           = GLFW_KEY_PAUSE,
+
+        MAX_ENUM = GLFW_KEY_LAST
     };
 
     /**
      * These have the same numeric value as the corresponding GLFW defines.
      *
-     * This is not an enum class because mods have to be combineable
+     * This is not an enum class because mods have to be combinable
      */
     enum eKeyMod {
-        shift = GLFW_MOD_SHIFT,
-        control = GLFW_MOD_CONTROL,
-        alt = GLFW_MOD_ALT,
-        super = GLFW_MOD_SUPER,
+        shift     = GLFW_MOD_SHIFT,
+        control   = GLFW_MOD_CONTROL,
+        alt       = GLFW_MOD_ALT,
+        super     = GLFW_MOD_SUPER,
+        caps_lock = GLFW_MOD_CAPS_LOCK,
+        num_lock  = GLFW_MOD_NUM_LOCK,
+
         MAX_ENUM
     };
 
@@ -94,16 +147,21 @@ namespace glb
      * These have the same numeric value as the corresponding GLFW defines.
      */
     enum class eMouseButton {
-        left = GLFW_MOUSE_BUTTON_LEFT,
-        right = GLFW_MOUSE_BUTTON_RIGHT,
-        wheel = GLFW_MOUSE_BUTTON_MIDDLE,
-        middle = wheel,
+        left     = GLFW_MOUSE_BUTTON_LEFT,
+        right    = GLFW_MOUSE_BUTTON_RIGHT,
+        wheel    = GLFW_MOUSE_BUTTON_MIDDLE,
+        middle   = wheel,
+
+        button_1 = GLFW_MOUSE_BUTTON_1,     // same as left
+        button_2 = GLFW_MOUSE_BUTTON_2,     // same as right
+        button_3 = GLFW_MOUSE_BUTTON_3,     // same as middle
         button_4 = GLFW_MOUSE_BUTTON_4,
-        button_5,
-        button_6,
-        button_7,
-        button_8,
-        MAX_ENUM
+        button_5 = GLFW_MOUSE_BUTTON_5,
+        button_6 = GLFW_MOUSE_BUTTON_6,
+        button_7 = GLFW_MOUSE_BUTTON_7,
+        button_8 = GLFW_MOUSE_BUTTON_8,
+
+        MAX_ENUM = GLFW_MOUSE_BUTTON_LAST
     };
 
     /**
@@ -113,6 +171,7 @@ namespace glb
         release = GLFW_RELEASE,
         press = GLFW_PRESS,
         repeat = GLFW_REPEAT,
+
         MAX_ENUM
     };
 } // namespace glb
